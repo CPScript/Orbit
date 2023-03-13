ef613d411095554d23d56d3d2efbb8d65af03a68
Chai <83523587+CPScript@users.noreply.github.com>
2023-03-13 19:13:56 -0400

    Update Install_Dependancies.py

--- Install_Dependancies.py
+++ Install_Dependancies.py
@@ -13,10 +13,15 @@ else:
 
 os.system(delet)
 time.sleep(1)
-print("Downloading Dependancys in 10 seconds")
+print("Downloading Dependancies in 10 seconds")
 print("Pleas wait...")
 time.sleep(10)
+
+# Installs requirments
 os.system(delet)
+call(["python", "tool/Install.py"])
+os.system(delet)
+
 
 #loading screen
 print("Loading.")
@@ -47,43 +52,59 @@ print("Loading...")
 time.sleep(1)
 os.system(delet)
 
+print("Done!")
+time.sleep(1)
+os.system(delet)
+print("Please choose what to use> 1 or 2")
+print("<1> GUI")
+print("<2> Terminal [recemended]")
+choice = input("")
 
-class co:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-print(co.OKGREEN + '[+] Installing Dependancies for DDoS tool' + co.ENDC)
-print(co.OKBLUE + '[+] Download GoLang from Golang.org/dl/'+ co.ENDC)
-try:
-    os.system('python3 -m pip install wheel')
-    os.system('python -m pip install wheel')
-    os.system('pip3 install wheel')
-    os.system('py -m pip install wheel')
-    os.system('python3 -m pip install pyfiglet')
-    os.system('pip3 install pyfiglet')
-    os.system('py -m pip install pyfiglet')
-    os.system('python -m pip install pyfiglet')
-    os.system('python3 -m pip install pyttsx3')
-    os.system('pip3 install pyttsx3')
-    os.system('py -m pip install pyttsx3')
-    os.system('python -m pip install pyttsx3')
-    os.system('python3 -m pip install colorama')
-    os.system('pip3 install colorama')
-    os.system('py -m pip install colorama')
-    os.system('python -m pip install colorama')
-    os.system('sudo apt-get install figlet')
-    print(
-        '''[+] If this comes and you are on any OS just Ignore only if there is a py command not found as below;
-          sh: py: command not found''')
-except:
-    print(co.BOLD + 'Just Install Pyfiglet, Pyttsx3 and GoLang and Colorama' + co.ENDC)
 
-time.sleep(5)
-print("done")
-call(["python", "Python-Script.py"])
+if choice == "GUI":
+    os.system(delet)
+    print("Loading.")
+    time.sleep(1)
+    os.system(delet)
+    print("Loading..")
+    time.sleep(1)
+    os.system(delet)
+    print("Loading...")
+    time.sleep(1)
+    os.system(delet)
+    print("Loading.")
+    time.sleep(1)
+    os.system(delet)
+    print("Loading..")
+    time.sleep(1)
+    os.system(delet)
+    print("Loading...")
+    time.sleep(1)
+    os.system(delet)
+    print("Done!")
+    time.sleep(5)
+    call(["python", "tool/GUI.py"])
+
+if choice == "terminal":
+    os.system(delet)
+    print("Loading.")
+    time.sleep(1)
+    os.system(delet)
+    print("Loading..")
+    time.sleep(1)
+    os.system(delet)
+    print("Loading...")
+    time.sleep(1)
+    os.system(delet)
+    print("Loading.")
+    time.sleep(1)
+    os.system(delet)
+    print("Loading..")
+    time.sleep(1)
+    os.system(delet)
+    print("Loading...")
+    time.sleep(1)
+    os.system(delet)
+    print("Done!")
+    time.sleep(5)
+    call(["python", "tool/DDoS.py"])
