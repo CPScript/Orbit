434264c3141a87f8b13ab8a37a01d51ac0fb263e
Chai <83523587+CPScript@users.noreply.github.com>
2023-03-14 19:39:24 -0400

    Update GUI.py

--- tool/GUI.py
+++ tool/GUI.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import tkinter as tk
+from tkinter import ttk 
 import os
 from tkinter import messagebox
 import subprocess
@@ -35,21 +36,36 @@ def Attack_Target():
        
     
 root = tk.Tk()
-root.title("Orbit | VERSION: BETA 0.6 ")
+root.title("Orbit | VERSION: 1.6 ")
+root.configure(bg='black')
+root.geometry("385x275")
+
+# Image
+bg = PhotoImage(file="background.png")
+
+# Label
+my_label = Label(root, image=bg)
+my_label.place(x=50, y=50, relwidth=1, relheight=1)
+
+
 
 Information = Label(text = "Orbit-DDoS", font = 'Calbri')
 Information.grid(row =1, column =1)
-Usage = Label(text = 'Usage: Enter the Website to DDoS and Number of Threads (Can be from 1024 to a billion) - (On Windows 1024 Threads are Limited)')
+Usage = Label(text = 'Made by: Chai-Programming')
 Usage.grid(row =2, column =1)
 Website_Name = Label(text = "Website to DDoS")
 Website_Name.grid(row = 3, column =1)
 Website = tk.Entry(root,bd = 5)
 Website.grid(row =4, column =1)
-Thread_Name = Label(text = "Number of Threads to use")
+Thread_Name = Label(text = "Number of Threads (1024 to Infinity) [requires a powerfull pc to do a lot]")
 Thread_Name.grid(row = 5, column =1)
 Thread = tk.Entry(root,bd = 5)
 Thread.grid(row = 6, column =1)
 
-Attack_Button = Button(text = '¿Attack?', font = 'Calbri', bd = 5, command = Attack_Target)
+
+Attack_Button = Button(text = '¿Attack?', font = 'Impact', bd = 5, command = Attack_Target)
+Attack_Button.configure(bg='red')
+
 Attack_Button.grid(row = 7, column =1)
+
 root.mainloop()
