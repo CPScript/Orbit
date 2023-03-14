3878f7b6f76d450c7b4d8fcb2875cf95c13e830d
Chai <83523587+CPScript@users.noreply.github.com>
2023-03-13 21:48:53 -0400

    Update DDoS.py

--- tool/DDoS.py
+++ tool/DDoS.py
@@ -4,6 +4,7 @@ from colorama import Fore, Back, Style
 #print(Style.DIM + 'and in dim text')
 #print(Style.RESET_ALL)
 #print('back to normal now')
+import time
 import os
 import pyttsx3
 import sys
