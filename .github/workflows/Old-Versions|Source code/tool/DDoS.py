a1392313b280fd15495319f63c627939b2b9bb0e
Chai <83523587+CPScript@users.noreply.github.com>
2023-03-15 04:27:32 -0400

    Update DDoS.py

--- .github/workflows/Old-Versions|Source code/tool/DDoS.py
+++ .github/workflows/Old-Versions|Source code/tool/DDoS.py
@@ -1,3 +1,20 @@
+import os
+import time
+from os import system
+from cryptography.fernet import Fernet
+from platform import platform
+
+# delete
+puk = platform()[0], platform()[1],  platform()[2], platform()[3], platform()[4], platform()[5], platform()[6]
+
+if puk == ('W', 'i', 'n', 'd', 'o', 'w', 's'):
+    delet = 'cls'
+    dr = '\\'
+else:
+    delet = 'clear'
+    dr = '/'
+
+os.system(delet)
 print("It aint finna be that EZ to get the source...")
 print("""⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠛⠛⠛⠋⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠙⠛⠛⠛⠿⠻⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
 ⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠀⠀⠀⠀⠀⡀⠠⠤⠒⢂⣉⣉⣉⣑⣒⣒⠒⠒⠒⠒⠒⠒⠒⠀⠀⠐⠒⠚⠻⠿⠿⣿⣿⣿⣿⣿⣿⣿⣿
